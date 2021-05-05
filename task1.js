dc398c6ac209eea1f2dfdc63b86cfd88cb2aee13
sauravraw <saurav.upadhyay@raweng.com>
2021-05-05 18:20:03 +0530

    task1 added

--- task1.js
+++ task1.js
@@ -1,7 +1,18 @@
 var a = 123;
 
 function abc() {
-  console.log(a);
+  console.log("Value of a: ", a);
   var a = 345;
 }
 abc();
+
+function pqr() {
+  var a = 345;
+  console.log("Value of a: ", a);
+}
+pqr();
+
+function xyz() {
+  console.log("Value of a: ", a);
+}
+xyz();
